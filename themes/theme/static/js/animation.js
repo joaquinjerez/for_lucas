724bcb942dbf8b0b722c82e14878f350a9070ffd
joaquinjerez <37566014+joaquinjerez@users.noreply.github.com>
2020-08-29 17:55:19 +0200

    first commit

---
+++ themes/theme/static/js/animation.js
@@ -0,0 +1,24 @@
+// const svgPath = document.querySelectorAll('.h1');
+
+// const svgText = anime({
+//   targets: svgPath,
+//   loop: false,
+//   direction: 'alternate',
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   duration: 500,
+//   delay: (el, i) => { return i * 200 }
+// }); 
+ 
+ 
+// const svgPath2 = document.querySelectorAll('.h2');
+
+// const svgText2 = anime({
+//   targets: svgPath2,
+//   loop: false,
+//   direction: 'alternate',
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   duration: 500,
+//   delay: (el, i) => { return i * 200 }
+// }); 
